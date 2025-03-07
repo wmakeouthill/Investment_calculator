c2859c3b3ca35a05bea4914457954174bb4f17a9
Wesley de Carvalho Augusto Correia <wcacorreia1995@gmail.com>
2025-03-07 19:58:01 +0000

    new file:   .editorconfig 	new file:   .gitignore 	modified:   README.md 	new file:   angular.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/investment-calculator-logo.png 	new file:   src/app/app.component.html 	new file:   src/app/app.component.ts 	new file:   src/app/header/header.component.css 	new file:   src/app/header/header.component.html 	new file:   src/app/header/header.component.ts 	new file:   src/app/investment-results/investment-results.component.css 	new file:   src/app/investment-results/investment-results.component.html 	new file:   src/app/investment-results/investment-results.component.ts 	new file:   src/app/user-input/user-input.component.css 	new file:   src/app/user-input/user-input.component.html 	new file:   src/app/user-input/user-input.component.ts 	new file:   src/index.html 	new file:   src/investment-results.ts 	new file:   src/main.ts 	new file:   src/styles.css 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.spec.json

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+}
