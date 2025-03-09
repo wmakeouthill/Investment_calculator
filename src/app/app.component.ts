01f47683a885eca0d21504683750ed4a2576390f
Wesley de Carvalho Augusto Correia <wcacorreia1995@gmail.com>
2025-03-09 08:33:38 +0000

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/header/header.component.html 	modified:   src/app/header/header.component.ts 	new file:   src/app/investment-input.model.ts 	modified:   src/app/investment-results/investment-results.component.html 	modified:   src/app/investment-results/investment-results.component.ts 	new file:   src/app/investment.service.ts 	modified:   src/app/user-input/user-input.component.html 	modified:   src/app/user-input/user-input.component.ts 	modified:   src/main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [InvestmentResultsComponent, UserInputComponent, HeaderComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  // resultsData?: {
+  //   year: number,
+  //   totalAmountInvested: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number
+  // }[];
+}
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  standalone: true,
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-
+  private investmentService = inject(InvestmentService);
+  results = computed(() => this.investmentService.resultData());
 }
