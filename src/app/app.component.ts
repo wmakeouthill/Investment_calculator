c40c43b15bdc26df6c98c35baf746abe51beee37
Wesley de Carvalho Augusto Correia <wcacorreia1995@gmail.com>
2025-03-12 22:43:58 +0000

    modified:   src/app/app.component.ts 	new file:   src/app/app.module.ts 	modified:   src/app/header/header.component.ts 	modified:   src/app/investment-results/investment-results.component.ts 	modified:   src/app/user-input/user-input.component.ts 	new file:   src/app/user-input/user-input.module.ts 	modified:   src/main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [InvestmentResultsComponent, UserInputComponent, HeaderComponent],
+  standalone: false,
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
